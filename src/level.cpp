5eee20645c6c3cdcdf41355131a1e08e172d555c
funnydog <funnydog@users.noreply.github.com>
2024-12-17 07:24:45 +0100

    Improve the code that loads the levels.

--- src/level.cpp
+++ src/level.cpp
@@ -39,42 +39,34 @@ GameLevel::Load(TextureHolder& textures, const char *path, GLuint levelWidth, GL
 
 	glm::vec2 pos(0.0f);
 	glm::vec2 size(unit_width, unit_height);
-	for (decltype(height) y = 0; y < height; ++y)
+	for (decltype(height) y = 0; y < height; ++y, pos.y += unit_height)
 	{
-		for (decltype(width) x = 0; x < width; ++x)
+		pos.x = 0.f;
+		for (decltype(width) x = 0; x < width; ++x, pos.x += unit_width)
 		{
-			if (tileData[y][x] == 1)
+			glm::vec3 color;
+			bool isSolid = false;
+			TextureID textureID = TextureID::Block;
+
+			switch (tileData[y][x])
 			{
-				// solid block
-				GameObject obj(
-					pos, size,
-					textures.get(TextureID::BlockSolid),
-					glm::vec3(0.8f, 0.8f, 0.7f));
-				obj.IsSolid = true;
-				Bricks.push_back(obj);
+			case 0: continue; // no brick, skip
+			case 1: color = glm::vec3(0.8f, 0.8f, 0.7f);
+				isSolid = true;
+				textureID = TextureID::BlockSolid;
+				break;
+			case 2: color = glm::vec3(0.2f, 0.6f, 1.0f); break;
+			case 3: color = glm::vec3(0.0f, 0.7f, 0.0f); break;
+			case 4: color = glm::vec3(0.8f, 0.8f, 0.4f); break;
+			case 5: color = glm::vec3(1.0f, 0.5f, 0.0f); break;
+			default: color = glm::vec3(1.0f); break;
 			}
-			else if (tileData[y][x] > 1)
-			{
-				// normal block
-				glm::vec3 color;
-				switch (tileData[y][x])
-				{
-				case 2: color = glm::vec3(0.2f, 0.6f, 1.0f); break;
-				case 3: color = glm::vec3(0.0f, 0.7f, 0.0f); break;
-				case 4: color = glm::vec3(0.8f, 0.8f, 0.4f); break;
-				case 5: color = glm::vec3(1.0f, 0.5f, 0.0f); break;
-				default: color = glm::vec3(1.0f); break;
-				}
-				GameObject obj(
-					pos, size,
-					textures.get(TextureID::Block),
-					color);
-				Bricks.push_back(obj);
-			}
-			pos.x += unit_width;
+
+			// insert the brick
+			auto &brick = Bricks.emplace_back(
+				pos, size, textures.get(textureID), color);
+			brick.IsSolid = isSolid;
 		}
-		pos.x = 0;
-		pos.y += unit_height;
 	}
 	return 0;
 }
