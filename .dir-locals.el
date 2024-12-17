82337094afa44c08128228a6fb8cc19ed7060f23
funnydog <funnydog@users.noreply.github.com>
2024-12-17 06:14:08 +0100

    Add some tool configuration in .dir-locals.el.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((eval . (setq flycheck-clang-include-path
+		       (list "/usr/include/freetype2")))))
+ (c++-mode . ((flycheck-clang-language-standard . "c++20")
+	      (flycheck-gcc-language-standard . "c++20"))))
